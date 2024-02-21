765e99811191acfad792901df7af83dbe4ec2b31
ashwindev58 <ashwindev58@gmail.com>
2024-02-21 19:14:10 +0530

    skills created success fully

--- lib/screens/sec4skill/sec4skill.dart
+++ lib/screens/sec4skill/sec4skill.dart
@@ -9,22 +9,106 @@ class Sec4Skill extends StatefulWidget {
 }
 
 class Sec4SkillState extends State<Sec4Skill> {
+  var flutterskill = [
+    'Developing Cross-Platform (Android/iOS) Apps',
+    'Flutter Bloc ',
+    'Version Controll Using GitHub',
+    'Flutter Web Development & Deployment'
+        'Widget building ',
+    'UI/UX design ',
+    'State management',
+    'Flutter animations',
+    'Flutter navigation',
+    'Dart programming language',
+    'Flutter packages/plugins integration',
+    'Responsive design',
+    'Deployment to App Store and Play Store',
+    'Integration with backend services',
+    'Knowledge of Dart\'s asynchronous programming',
+    'Understanding Flutter\'s widget lifecycle',
+    'Flutter performance optimization',
+    'Integration of third-party libraries in Flutter',
+  ];
+
+  var otherSkills = [
+    'Microsoft Office )',
+    'Appstore and Play Store Deployment',
+    'Bard AI',
+    'Firebase',
+    'HTTP API integration',
+    'PHP CodeIgniter (Back end only)',
+    'SQL (mentioned twice)',
+    'Problem management',
+    'Time management',
+    'AI Prompt',
+    'Positive attitude',
+    'ChatGPT',
+    'Gemini',
+    'Bard',
+    'Git',
+    'JSON',
+  ];
+
+  var softSkills = [
+    "Positive attitude",
+    "Problem-solving skills",
+    "Time management",
+    "Effective communication",
+    "Team collaboration",
+    "Adaptability",
+    "Creativity",
+    "Critical thinking",
+    "Attention to detail",
+    "Decision-making",
+    "Emotional intelligence",
+    "Open-mindedness",
+    "Leadership",
+    "Interpersonal skills",
+    "Self-motivation",
+    "Conflict resolution",
+    "Empathy",
+    "Stress management",
+    "Professionalism",
+    "Active listening"
+  ];
+
+  var proList = [
+    'Java',
+    'C',
+    'Python',
+    'Dart',
+    'C++',
+    'PHP',
+    'Oops',
+    'Data Structures',
+    'LeetCOde',
+    'Functional Programming',
+    'Code Optimization',
+    'Version Control '
+        'MVC Architecture',
+    'Databases (SQL,MySql,Sqlite)'
+  ];
+
   @override
   void initState() {
-    constraints=widget.constraints;
+    constraints = widget.constraints;
     super.initState();
   }
+
   late BoxConstraints constraints;
   @override
   Widget build(BuildContext context) {
-    
+    //   print(constraints.maxWidth);
     return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: constraints.maxHeight * 0.1,
-        horizontal: constraints.maxWidth * 0.1,
+      padding: EdgeInsets.only(
+        top: constraints.maxHeight * 0.1,
+        bottom: constraints.maxHeight * 0.1,
+        left: constraints.maxWidth * 0.1,
+        right: constraints.maxWidth * 0.05,
       ),
-      child: Row(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           SizedBox(
             width: constraints.maxWidth * 0.35,
@@ -44,7 +128,7 @@ class Sec4SkillState extends State<Sec4Skill> {
                   height: constraints.maxHeight * 0.02,
                 ),
                 const Text(
-                  "A creative and dedicated Flutter developer",
+                  "A committed and passionate Flutter developer",
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 32,
@@ -57,9 +141,145 @@ class Sec4SkillState extends State<Sec4Skill> {
               ],
             ),
           ),
-          
-          
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SkillWidget(
+                  constraints: constraints,
+                  skillTittle: "Programming Languages",
+                  skilList: proList,
+                ),
+                SkillWidget(
+                    constraints: constraints,
+                    skillTittle: "Flutter Skills",
+                    skilList: flutterskill),
+                SkillWidget(
+                    constraints: constraints,
+                    skillTittle: "Soft Skills",
+                    skilList: softSkills),
+                SkillWidget(
+                    constraints: constraints,
+                    skillTittle: "Others",
+                    skilList: otherSkills),
+              ],
+            ),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class SkillWidget extends StatefulWidget {
+  final String skillTittle;
+
+  const SkillWidget({
+    super.key,
+    required this.constraints,
+    required this.skilList,
+    required this.skillTittle,
+  });
+
+  final BoxConstraints constraints;
+  final List<String> skilList;
+
+  @override
+  State<SkillWidget> createState() => _SkillWidgetState();
+}
+
+class _SkillWidgetState extends State<SkillWidget> {
+  bool isonTop = false;
+
+  bool isVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onExit: (e) {
+        setState(() {
+          isonTop = false;
+        });
+      },
+      onEnter: (e) {
+        setState(() {
+          isonTop = true;
+        });
+      },
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            isVisible = !isVisible;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 250),
+          decoration: BoxDecoration(
+            // border: Border.all(
+            //   color: isonTop ? Colors.deepPurple : Colors.grey.withOpacity(0.6),
+            //   width: isonTop ? 2 : 1.0, // Adjust border width as needed
+            // ),
+            borderRadius: BorderRadius.circular(
+              ((widget.constraints.maxHeight * widget.constraints.maxWidth) /
+                      100) *
+                  0.0011,
+            ),
+          ),
+          padding: EdgeInsets.all(
+              ((widget.constraints.maxHeight * widget.constraints.maxWidth) /
+                      100) *
+                  0.0031),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                widget.skillTittle,
+                style: TextStyle(
+                  color: isVisible
+                      ? const Color.fromRGBO(103, 58, 183, 1)
+                      : isonTop
+                          ? const Color.fromRGBO(103, 58, 183, 1)
+                          : Colors.grey,
+                  fontSize: 19,
+                  fontWeight: isVisible
+                      ? FontWeight.w900
+                      : isonTop
+                          ? FontWeight.w900
+                          : FontWeight.w500,
+                ),
+              ),
+              Visibility(
+                visible: isVisible,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: widget.constraints.maxHeight * 0.01,
+                    ),
+                    for (int i = 0; i < widget.skilList.length; i++)
+                      Container(
+                        padding: EdgeInsets.only(
+                          top: widget.constraints.maxHeight * 0.01,
+                        ),
+                        child: Text(
+                          "  ${i + 1}. ${widget.skilList[i]}",
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/section2/widgetsection2.dart
+++ lib/screens/section2/widgetsection2.dart
@@ -23,68 +23,65 @@ class WidgetSection2 extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-             SizedBox(
-            width: constraints.maxWidth * 0.35,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "#about_me",
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "#about_me",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: constraints.maxHeight * 0.02,
+              ),
+              const Text(
+                "A creative and dedicated\n Flutter developer",
+
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(height: constraints.maxHeight*0.05,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularImageButton(
+                    constraints: constraints,siteUrl:"https://profile.indeed.com/p/ashwink-b47l20m",
+                    imageurl:
+                        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAflBMVEUAAAD///////////////////////////////////////////////+/zuZvkMZQd7pAa7Sgtdnv8/gAOptgg8AgUqcgU6evweCAnc0QRqF/nc3v8vhwkMcwXq3f5vLP2uz////f5/JfhMB/nM0wX66AnM1PeLqPqdOftdrv8/mN6/jEAAAAIXRSTlMAEGCfz+//XyCQj98w/////////////////////////6AQ1BAWAAABD0lEQVR4AYWThRZCIQxAR+11h939/z+oHECHGNd4sbvR8IJxIRWikiJiEBInCl+IGHxYim+kfnqGARkpkj/jRVnVVdMaIw/yi7qr+7IaxolXg7n4dDZfGHEojGFG4/q3XK3RsSE9jV3+SqevC6ToRrbmdjc84m3ddeOOCAkAQ8Oo6++7BzURFANuB3DQ/8dOgwTuWjjpAnjW8YEKAqS5KU0dLVyoIEGZYdnOr8veH4YCN+5vWGGBVtxc68IXFFLqrus8QYH8LUjY/hYE8N9CBEz9FBhA8ksQZLmpQJeblAiFxNtyoZCxYNMvu252CzY+2fa3fVU84/nfg0NgyXs8YeATb0lUJTGEML61x5+T7DuGyRy8BcS3+wAAAABJRU5ErkJggg==',
+
                   ),
-                ),
-                SizedBox(
-                  height: constraints.maxHeight * 0.02,
-                ),
-                const Text(
-                  "Proficient Flutter Developer with 1+ Years of Successful App Delivery",
-            
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.w900,
+                  SizedBox(width: constraints.maxWidth*0.015,),
+                  CircularImageButton(
+                    constraints: constraints,siteUrl: "https://api.whatsapp.com/send/?phone=918086850403",
+                    imageurl:
+                        'https://raw.githubusercontent.com/ashwindev58/images/73bb69b936884bb504ef3c7602a3de460fbc9e7a/whatsap.png',
+
                   ),
-                ),
-                SizedBox(height: constraints.maxHeight*0.05,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularImageButton(
-                      constraints: constraints,siteUrl:"https://profile.indeed.com/p/ashwink-b47l20m",
-                      imageurl:
-                          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAflBMVEUAAAD///////////////////////////////////////////////+/zuZvkMZQd7pAa7Sgtdnv8/gAOptgg8AgUqcgU6evweCAnc0QRqF/nc3v8vhwkMcwXq3f5vLP2uz////f5/JfhMB/nM0wX66AnM1PeLqPqdOftdrv8/mN6/jEAAAAIXRSTlMAEGCfz+//XyCQj98w/////////////////////////6AQ1BAWAAABD0lEQVR4AYWThRZCIQxAR+11h939/z+oHECHGNd4sbvR8IJxIRWikiJiEBInCl+IGHxYim+kfnqGARkpkj/jRVnVVdMaIw/yi7qr+7IaxolXg7n4dDZfGHEojGFG4/q3XK3RsSE9jV3+SqevC6ToRrbmdjc84m3ddeOOCAkAQ8Oo6++7BzURFANuB3DQ/8dOgwTuWjjpAnjW8YEKAqS5KU0dLVyoIEGZYdnOr8veH4YCN+5vWGGBVtxc68IXFFLqrus8QYH8LUjY/hYE8N9CBEz9FBhA8ksQZLmpQJeblAiFxNtyoZCxYNMvu252CzY+2fa3fVU84/nfg0NgyXs8YeATb0lUJTGEML61x5+T7DuGyRy8BcS3+wAAAABJRU5ErkJggg==',
-            
-                    ),
-                    SizedBox(width: constraints.maxWidth*0.015,),
-                    CircularImageButton(
-                      constraints: constraints,siteUrl: "https://api.whatsapp.com/send/?phone=918086850403",
-                      imageurl:
-                          'https://raw.githubusercontent.com/ashwindev58/images/73bb69b936884bb504ef3c7602a3de460fbc9e7a/whatsap.png',
-            
-                    ),
-                    SizedBox(width: constraints.maxWidth*0.015,),
-                    CircularImageButton(
-                      constraints: constraints,siteUrl: "https://www.linkedin.com/in/ashwin-k-759a82227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-                      imageurl:
-                          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAA4CAMAAACfWMssAAAAdVBMVEX9+vX///f++/X09fMAWr/W4OsAYcH5+PQAVr56nNMATLsAXL8AUb3///nw8vIAX8CMqdfq7fAASLsKZsKkut3C0OWZs9rj6O6Vr9kAQbmuwd+8y+NFfsl+oNTW3Olqkc8uc8YAObddis0ebMQ9eMdPg8pul9FesxvkAAABsUlEQVRIie2TbXOjIBRGuYBioiIoLykGsWbj//+JCzZpm5om2/22sx5nmLngQZAHhDY2Nv4X4B28Gks9+NKNb4YxhsPugmdfTUkwwqm5FjdDtBmbPFFltyImrwYQbidIBXsx8DHEWgPU1acuQVfiXgGCPlv2wSqVdpOe1FAFzS/R0JLS/BvxbNRxzgVUjo8+G+mE2FSGXAEVtpGOy2NxX+yEKQ6hkoUZR1IY2RhBnSiiaOoSpAC93uM+rS6KtNZ7W+aFHeipa8Ix1HV+EU1rh/1KrHa9k1EsIYqFOh35XhGHp87q6xdNdU9si6p1nT9UoF9sq4ZWuKooMpu3p9IsYq3uiAiRCCOMEcwIIgwTDsSmJFgcK5p3TZc363NM8YgvLU3MEF7C81akCpS4skrOYx5k9WcTfcCGW5PZP5sJ85jSGK/3NXG/hO7eNfoiBoA+9DyI3njlvXa9CFjNgjw2k8i6/jRx688ocJHNInNmV5/5M/Es5WjVnGk/1aEWwyykUwEdn4iIHQ4u85oJpXvosZa95oPV5qmI3i5g/B3LieMF8POnG/0T4mx/5W1sbPwb/AaC4RxEdWUvFgAAAABJRU5ErkJggg==',
-            
-                    ),
-                    SizedBox(width: constraints.maxWidth*0.015,),
-            
-                     CircularImageButton(
-                      constraints: constraints,siteUrl: "https://github.com/ashwindev58",
-                      imageurl:
-                          'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
-            
-                    ),
-                  ],
-                )
-              ],
-            ),
+                  SizedBox(width: constraints.maxWidth*0.015,),
+                  CircularImageButton(
+                    constraints: constraints,siteUrl: "https://www.linkedin.com/in/ashwin-k-759a82227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+                    imageurl:
+                        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAA4CAMAAACfWMssAAAAdVBMVEX9+vX///f++/X09fMAWr/W4OsAYcH5+PQAVr56nNMATLsAXL8AUb3///nw8vIAX8CMqdfq7fAASLsKZsKkut3C0OWZs9rj6O6Vr9kAQbmuwd+8y+NFfsl+oNTW3Olqkc8uc8YAObddis0ebMQ9eMdPg8pul9FesxvkAAABsUlEQVRIie2TbXOjIBRGuYBioiIoLykGsWbj//+JCzZpm5om2/22sx5nmLngQZAHhDY2Nv4X4B28Gks9+NKNb4YxhsPugmdfTUkwwqm5FjdDtBmbPFFltyImrwYQbidIBXsx8DHEWgPU1acuQVfiXgGCPlv2wSqVdpOe1FAFzS/R0JLS/BvxbNRxzgVUjo8+G+mE2FSGXAEVtpGOy2NxX+yEKQ6hkoUZR1IY2RhBnSiiaOoSpAC93uM+rS6KtNZ7W+aFHeipa8Ix1HV+EU1rh/1KrHa9k1EsIYqFOh35XhGHp87q6xdNdU9si6p1nT9UoF9sq4ZWuKooMpu3p9IsYq3uiAiRCCOMEcwIIgwTDsSmJFgcK5p3TZc363NM8YgvLU3MEF7C81akCpS4skrOYx5k9WcTfcCGW5PZP5sJ85jSGK/3NXG/hO7eNfoiBoA+9DyI3njlvXa9CFjNgjw2k8i6/jRx688ocJHNInNmV5/5M/Es5WjVnGk/1aEWwyykUwEdn4iIHQ4u85oJpXvosZa95oPV5qmI3i5g/B3LieMF8POnG/0T4mx/5W1sbPwb/AaC4RxEdWUvFgAAAABJRU5ErkJggg==',
+
+                  ),
+                  SizedBox(width: constraints.maxWidth*0.015,),
+
+                   CircularImageButton(
+                    constraints: constraints,siteUrl: "https://github.com/ashwindev58",
+                    imageurl:
+                        'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
+
+                  ),
+                ],
+              )
+            ],
           ),
           SizedBox(
             width: constraints.maxWidth * 0.1,
